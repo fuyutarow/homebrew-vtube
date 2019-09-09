97a6f603f685dac4815c21e3634a7da136601588
fuyutarow <fuyutarow@gmail.com>
2019-09-09 22:20:12 +0900

    Add Formula

---
+++ Formula/vtube.rb
@@ -0,0 +1,11 @@
+class Vtube < Formula
+  desc "music bot CLI like Rythm(Discord bot)"
+  homepage "https://github.com/fuyutarow/vtube"
+  url "https://github.com/fuyutarow/vtube/archive/2019.09.09.tar.gz"
+  sha256 "264eed3873895eec34bc498b6825d41fc8a5c32e178c835998d64788e7a7e25a"
+
+  def install
+    bin.install "bin/vtube"
+  end
+end
+
