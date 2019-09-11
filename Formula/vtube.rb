75581a5b96fe432461109a480965ea112fca2d65
fuyutarow <fuyutarow@gmail.com>
2019-09-11 21:58:29 +0900

    Up to 2019.09.11

--- Formula/vtube.rb
+++ Formula/vtube.rb
@@ -1,8 +1,7 @@
 class Vtube < Formula
   desc "music bot CLI like Rythm(Discord bot)"
   homepage "https://github.com/fuyutarow/vtube"
-  url "https://github.com/fuyutarow/vtube/archive/2019.09.09.tar.gz"
-  sha256 "264eed3873895eec34bc498b6825d41fc8a5c32e178c835998d64788e7a7e25a"
+  url "https://github.com/fuyutarow/vtube/archive/2019.09.11.tar.gz"
 
   depends_on "youtube-dl"
   depends_on "mplayer"
